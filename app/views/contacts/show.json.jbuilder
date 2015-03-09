5047ffa185ad1b43d154f839103c47389409229e
Javier Silva <jsilva4@hawk.iit.edu>
2015-03-09 11:53:02 -0500

    rails g scaffold Contact firstName:string lastName:string email:string phoneNumber:biginteger address:string

---
+++ app/views/contacts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @contact, :id, :firstName, :lastName, :email, :phoneNumber, :address, :created_at, :updated_at
---
+++ db/migrate/20150309165148_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :firstName
+      t.string :lastName
+      t.string :email
+      t.biginteger :phoneNumber
+      t.string :address
+
+      t.timestamps null: false
+    end
+  end
+end
