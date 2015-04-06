1dda6e23d6e4dc44bb043a0ea043430b11180eca
Javier Silva <jsilva4@hawk.iit.edu>
2015-04-05 20:40:37 -0500

    added datat to the seeds.rb file and then ran  rake db:reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+contactlist = [
+    ["Javier", "Silva", "jsilva4@hawk.iit.edu", "asdbasdbsad", "23123123123"],
+    ["Jay", "Silva", "jay4@hawk.iit.edu", "asdbasdbsad", "23123123123"],
+    ["Xin", "Chen", "xchen4@hawk.iit.edu", "asdbasdbsad", "23123123123"],
+    ["steve", "burnette", "sburnette12@hawk.iit.edu", "asdbasdbsad", "23123123123"],
+    ["Neeraj", "Soparawala", "neeraj@hawk.iit.edu", "asdbasdbsad", "23123123123"]
+
+]
+
+contactlist.each do |firstName, lastName, email, phoneNumber, address|
+    Contact.create(firstName: firstName, lastName: lastName, email: email, phoneNumber: phoneNumber, address: address)
+end
