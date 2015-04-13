444e5d9612945cc21fcb488d620d13a1daf3b8ab
Javier Silva <jsilva4@hawk.iit.edu>
2015-04-12 19:38:43 -0500

    added nickname to seeds.rb and rake db:reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 contactlist = [
-    ["Javier", "Silva", "jsilva4@hawk.iit.edu", "3020 N. Allen", "773-886-7087"],
-    ["Jav", "Silva", "silva_javier@att.net", "3020 N. Allen", "23123123123"],
-    ["Xin", "Chen", "xchen4@hawk.iit.edu", "asdbasdbsad", "23123123123"],
-    ["steve", "burnette", "sburnette12@hawk.iit.edu", "asdbasdbsad", "23123123123"],
-    ["Neeraj", "Soparawala", "neeraj@hawk.iit.edu", "asdbasdbsad", "23123123123"]
+    ["Javier", "Silva", "jsilva4@hawk.iit.edu", "3020 N. Allen", "773-886-7087", "Jav"],
+    ["Jav", "Silva", "silva_javier@att.net", "3020 N. Allen", "23123123123", "Javi"],
+    ["Xin", "Chen", "xchen4@hawk.iit.edu", "asdbasdbsad", "23123123123", "Chen"],
+    ["steve", "burnette", "sburnette12@hawk.iit.edu", "asdbasdbsad", "23123123123", "Stevo"],
+    ["Neeraj", "Soparawala", "neeraj@hawk.iit.edu", "asdbasdbsad", "23123123123", "Nee"]
 
 ]
 
-contactlist.each do |firstName, lastName, email, phoneNumber, address|
-    Contact.create(firstName: firstName, lastName: lastName, email: email, phoneNumber: phoneNumber, address: address)
+contactlist.each do |firstName, lastName, email, phoneNumber, address, nickname|
+    Contact.create(firstName: firstName, lastName: lastName, email: email, phoneNumber: phoneNumber, address: address, nickname: nickname)
 end
